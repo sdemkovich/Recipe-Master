fa89c5595c5c8847ab681557cc331f546f41cf89
Chris Bragg <cbragg9@yahoo.com>
2020-04-09 15:18:54 -0700

    nutrition ajax

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Project1
 
-Initial Commit
-
-## Testing Branch 2
+## Latest Commit  
+-Created temporary html tags that will display nutrient info  
+-Created temporary html buttons that hold recipe ingredients  
+-Created event handler on recipe buttons  
+-Created AJAX call to search food title of recipe button  
+-Created functions to handle AJAX response and update html tags with nutrient info  
--- script.js
+++ script.js
@@ -1,14 +1,109 @@
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://edamam-edamam-nutrition-analysis.p.rapidapi.com/api/nutrition-data?ingr=1%20large%20apple",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "edamam-edamam-nutrition-analysis.p.rapidapi.com",
-		"x-rapidapi-key": "d8b37011d5mshfdc852418e9e300p167785jsn0d94302f0b12"
-	}
-}
+$(document).ready(function(){ 
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
+  // U.S. DEPARTMENT OF AGRICULTURE 
+  // FoodData Central API 
+
+  // When a food ingredient button is clicked
+  $(".button").on("click", function() {
+
+    // The button title will be the query food
+    var ingredientQuery = $(this).text();
+  
+    // Settings for AJAX call
+    var fdc_api_key = "RlTgUNeWpu2FIFPDd0AmRrHssiC7e96O5TKQSGEc";
+    var dataType = "Branded"
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://api.nal.usda.gov/fdc/v1/foods/search?api_key=" + fdc_api_key + "&query=" + ingredientQuery + "&dataType=" + dataType,
+      "method": "GET",
+    }
+
+    // Ajax call
+    $.ajax(settings).done(function (response) {
+
+      // Update HTML to display what was searched
+      var firstFoodReturned = response.foods[0].description;
+      $("#ingredient-name").text("API called for: " + firstFoodReturned);
+
+      var nutrientValue = "";
+
+      // Information for 5 nutrients that will be displayed
+      var nutrientsArray = [
+        {"nutrientName": "Energy",
+        "nutrientHTML": "#calories-tag",
+        "nutrientDisplay": "Calories: "},
+        {"nutrientName": "Protein",
+        "nutrientHTML": "#protein-tag",
+        "nutrientDisplay": "Protein: "},
+        {"nutrientName": "Carbohydrate, by difference",
+        "nutrientHTML": "#carbs-tag",
+        "nutrientDisplay": "Carbs: "},
+        {"nutrientName": "Sugars, total including NLEA",
+        "nutrientHTML": "#sugars-tag",
+        "nutrientDisplay": "Sugar: "},
+        {"nutrientName": "Total lipid (fat)",
+        "nutrientHTML": "#fat-tag",
+        "nutrientDisplay": "Fat: "},
+      ];
+
+
+      // Loop through food nutrients array for the first food item from the response
+      for (var i = 0; i < response.foods[0].foodNutrients.length; i++) {
+        updateNutrient();
+      } 
+
+      // Updates the HTML tags for each nutrient
+      function updateNutrient() {
+        for (var j = 0; j < nutrientsArray.length; j++) {
+          var currentNutrientName = nutrientsArray[j].nutrientName;
+          var currentNutrientDisplay = nutrientsArray[j].nutrientDisplay;
+          var currentNutrientHTML = nutrientsArray[j].nutrientHTML;
+
+          // Update the corresponding HTML with the current nutrient info
+          if (response.foods[0].foodNutrients[i].nutrientName === currentNutrientName) {
+            nutrientValue = response.foods[0].foodNutrients[i].value;
+
+            // Add grams units if the nutrient is not calories
+            if (currentNutrientName === "Energy") {
+              $(currentNutrientHTML).text(currentNutrientDisplay + nutrientValue);
+            } else {
+              $(currentNutrientHTML).text(currentNutrientDisplay + nutrientValue + " g");
+            }
+          }
+        }
+      }
+
+    });
+
+  })
+
 });
+
+// // Recipe puppy
+// var urlIngredients = "onions, garlic, chicken legs";
+// var pageNumber = "&p=1";
+
+// var settings = {
+// 	"url": "https://recipe-puppy.p.rapidapi.com/?i=" + urlIngredients + pageNumber,
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "recipe-puppy.p.rapidapi.com",
+// 		"x-rapidapi-key": "d8b37011d5mshfdc852418e9e300p167785jsn0d94302f0b12"
+// 	}
+// }
+
+// // AJAX Recipe Puppy
+// $.ajax(settings).then(function (response) {
+//   var recipePuppyResponse = JSON.parse(response);
+//   console.log(recipePuppyResponse);
+
+//   // List all ingredients 
+//   for (var i = 0; i < recipePuppyResponse.results.length; i++) {
+
+//     // Get title and remove carriage returns
+//     var recipeTitle = recipePuppyResponse.results[i].title.replace(/[\n\r]/g, '');
+//     console.log(recipeTitle);
+//   }
+
+// });
